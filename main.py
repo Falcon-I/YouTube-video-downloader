e357419b5cc916308ca2010c7ee3fda4f5299a1b
Falcon-I <76063025+Falcon-I@users.noreply.github.com>
2021-01-15 21:18:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from tqdm.auto import tqdm
 try:
     url = p.YouTube(input('Enter the url of the video : '))
     video = url.streams.get_by_itag(22)
-    print("Starting Download.....");video.download()
+    vpath = input("Enter the path to store the video : ")
+    print("Starting Download.....");video.download(vpath)
     for i in tqdm(range(int(9e6))):
         pass;
     print("Your video is Successfully Downloaded")
